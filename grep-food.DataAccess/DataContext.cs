b339f4d30d9a3c354a591a9191fded2fbc85c6b8
Benny99M <benescumatei@yahoo.com>
2021-04-27 17:02:10 +0300

    Added Entities and Data Access projects

---
+++ grep-food.DataAccess/DataContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace grep_food.DataAccess
+{
+    public class DataContext : DbContext, IDataRepository, IUnitOfWork
+    {
+        public DataContext(DbContextOptions<DataContext> contextOptions) : base(contextOptions)
+        {
+
+        }
+
+        public void Commit()
+        {
+            SaveChanges();
+        }
+
+        public void Delete<TEntity>(TEntity entity) where TEntity : class
+        {
+            Delete(entity);
+        }
+
+        public TEntity Insert<TEntity>(TEntity entity) where TEntity : class
+        {
+            return Insert(entity);
+        }
+
+        public IQueryable<TEntity> Query<TEntity>() where TEntity : class
+        {
+            return Set<TEntity>();
+        }
+
+        void IDataRepository.Update<TEntity>(TEntity entity)
+        {
+            Update(entity);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var registrar = this.GetService<IEntityTypeConfigurationRegistrar>();
+            registrar.ApplyConfiguration(modelBuilder);
+        }
+    }
+}
---
+++ grep-food.DataAccess/IDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace grep_food.DataAccess
+{
+    public interface IDataRepository
+    {
+        IQueryable<TEntity> Query<TEntity>() where TEntity : class;
+
+        void Delete<TEntity>(TEntity entity) where TEntity : class;
+
+        void Update<TEntity>(TEntity entity) where TEntity : class;
+
+        TEntity Insert<TEntity>(TEntity entity) where TEntity : class;
+    }
+}
