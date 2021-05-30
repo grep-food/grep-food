a1a7ce8daff8426950a8091782a5d7236f11ca69
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-05-30 15:24:11 +0300

    ingredient class

---
+++ grep-food.DomainEntities/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace grep_food.DomainEntities
+{
+    public class Ingredient
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        
+        //for the ingredientes functionality LATER 
+        //public bool Selected { get; set; }
+    }
+}
