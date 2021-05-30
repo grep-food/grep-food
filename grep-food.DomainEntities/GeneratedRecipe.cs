0eb9e93d6533801ae0fb864b4d40064a530e4e14
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-05-30 15:14:08 +0300

    GeneratedRecipe class

---
+++ grep-food.DomainEntities/GeneratedRecipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace grep_food.DomainEntities
+{
+    public class GeneratedRecipe
+    {
+        public Guid UserId { get; }
+        public Guid RecipeId { get; }
+
+        public Recipe GenerateRecipe(int id) {
+            Recipe _recipe = new Recipe();
+            //select random from db
+            return _recipe;
+        }
+    }
+}
