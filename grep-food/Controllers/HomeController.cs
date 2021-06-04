f6807826a3c230d4e2a57c84a31b7bac3b7a70d8
Benny99M <benescumatei@yahoo.com>
2021-06-04 22:16:08 +0300

    Added recipe controller and folder

--- grep-food/Controllers/HomeController.cs
+++ grep-food/Controllers/HomeController.cs
@@ -26,30 +26,13 @@ namespace grep_food.Controllers
 
         public IActionResult Index()
         {
-            var data2 = _dataRepository.Query<RecipeViewModel>().OrderBy(recipe=>recipe.Name).First();
-            //Console.WriteLine($"data: '{data2}' data-siz '{data2.Id}'");
+           
             
             
-            return Recipe(data2.Name);
-
-        }
-        public IActionResult Recipe(string name)
-        {
-            Console.WriteLine($"Retzipe neim: '{name}'");
-            var data = _dataRepository.Query<RecipeViewModel>().Take(10).ToArray();
-            return View(data.Select(x => new RecipeViewModel
-            {
-               
-                Id = x.Id,
-                Name = x.Name,
-                TimeMinutes = x.TimeMinutes,
-                Instructions = x.Instructions,
-                Image = x.Image
-
-            }));
-             
             return View();
+
         }
+     
 
         public IActionResult Privacy()
         {
@@ -60,7 +43,7 @@ namespace grep_food.Controllers
         {
             return View();
         }
-
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ grep-food/Controllers/RecipeController.cs
@@ -0,0 +1,47 @@
+﻿using grep_food.DataAccess;
+using grep_food.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace grep_food.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly IDataRepository _dataRepository;
+        public RecipeController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+        public IActionResult Index(Guid Id)
+        {
+            Console.WriteLine($"id:'{Id}'");
+            return View();
+        }
+
+        
+        
+        public IActionResult Recipe(string name)
+        {
+
+            var data = _dataRepository.Query<RecipeViewModel>().Take(10).ToArray();
+            return View(data.Select(x => new RecipeViewModel
+            {
+
+                Id = x.Id,
+                Name = x.Name,
+                TimeMinutes = x.TimeMinutes,
+                Instructions = x.Instructions,
+                Image = x.Image
+
+            }));
+
+            return View();
+        }
+
+
+
+    }
+}
