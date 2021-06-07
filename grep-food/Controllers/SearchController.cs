dbc1f5ede35d7ab937e8013487c2a816bc0be3c3
Mutescu Andrei-Ioan <amutescu@yahoo.com>
2021-06-07 23:57:49 +0300

    search

--- grep-food/Controllers/SearchController.cs
+++ grep-food/Controllers/SearchController.cs
@@ -5,23 +5,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using grep_food.DomainEntities;
 using grep_food.Models;
+using grep_food.DataAccess;
+using grep_food.DataAccess.Dto;
+
 namespace grep_food.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly IDataRepository _dataRepository;
+        //public HomeController(ILogger<HomeController> logger, IDataRepository dataRepository)
+        public SearchController(IDataRepository dataRepository)
+        {
+            // _logger = logger;
+            Console.WriteLine($"dataRepo: '{_dataRepository}'");
+            _dataRepository = dataRepository;
+        }
         [HttpPost]
         public IActionResult Index(List<BaseIngredientViewModel> Baseing)
         {
-                Console.WriteLine("length " + Baseing.Count());
-                foreach (var ids in Baseing)
+            List<BaseIngredientViewModel> BaseingChecked = new List<BaseIngredientViewModel>();
+            Console.WriteLine("length " + Baseing.Count());
+            foreach (var ids in Baseing)
+            {
+                Console.WriteLine("id: " + ids.Name + " select: " + ids.isChecked);
+                if (ids.isChecked)
+                    BaseingChecked.Add(ids);
+
+            }
+            foreach (var ids in BaseingChecked)
+            {
+                Console.WriteLine("id: " + ids.Name + " select: " + ids.isChecked);
+
+            }
+            List<RecipeDto> recipes=_dataRepository.Query<RecipeDto>().ToList();
+
+            foreach (var ids in BaseingChecked) { 
+                if (BaseingChecked.Count != 0)
                 {
-                    Console.WriteLine("id: " + ids.Name + " select: " + ids.isChecked);
-
-
-
-                }
-
-                return View();
+                    recipes = recipes.Where(x => x.Name.Equals("Eastern European Kotlety")).ToList();
+                    //recipes = recipes.Where(x => x.Name.Equals("Eastern European Kotlety")).ToList();
+                } 
+            }
+            return View(recipes);
         }
+
     }
 }
